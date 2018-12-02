e9398bb989aa9a557a273a1ce11e3a5ea34c1823
HIENVO <bachdienquandhcn@gmail.com>
2018-12-02 15:05:57 +0700

    aaa

--- app/src/main/java/com/giaitri24h/io/tangthuvien/MainActivity.java
+++ app/src/main/java/com/giaitri24h/io/tangthuvien/MainActivity.java
@@ -1,10 +1,15 @@
 package com.giaitri24h.io.tangthuvien;
 
 import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.TextView;
 
 import org.json.JSONException;
@@ -21,9 +26,22 @@ public class MainActivity extends AppCompatActivity {
         String strdata=Helper.loadJSONFromAsset(getApplicationContext() );
         try{
             JSONObject obj = new JSONObject(strdata);
-            TextView tv = (TextView)findViewById(R.id.titleBook); tv.setText(obj.getString("Name"));
+            TextView tv = (TextView)findViewById(R.id.titleBook);
+            tv.setText(obj.getString("Name"));
+            TextView descriptBook = (TextView)findViewById(R.id.descriptBook);
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+                descriptBook.setText(Html.fromHtml(obj.getString("Description"), Html.FROM_HTML_MODE_COMPACT));
+            } else {
+                descriptBook.setText(Html.fromHtml(obj.getString("Description")));
+            }
+            Button btnChapter2 = (Button)findViewById(R.id.buttonChapter2);
         }catch (JSONException e){
 
         }
+    }
+    public void buttonClick(View view){
+        Intent mintent = new Intent(MainActivity.this, ListChapter.class);
+        startActivity(mintent);
     }
 }
